35f96d798b1c108bf70803df8eceaa4849ca5048
Hyenath <nathan.dubaille@gmail.com>
2024-12-03 14:56:12 +0100

    js,css

---
+++ mapCDI/main.js
@@ -0,0 +1,4 @@
+// JavaScript pour ajouter de l'interactivité
+document.getElementById("contactButton").addEventListener("click", function () {
+    alert("Merci de vouloir nous contacter. Envoyez-nous un email à cdi@exemple.com !");
+});
